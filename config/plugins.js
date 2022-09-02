d1dfac09e9748795329fb3c70086ba188b6abc61
Nagy Istvan <info@ist1.dev>
2022-09-02 22:50:01 +0200

    pre final version

--- config/plugins.js
+++ config/plugins.js
@@ -2,4 +2,29 @@ module.exports = ({ env }) => ({
   "generate-data": {
     enabled: true,
   },
+  "preview-button": {
+    enabled: true,
+    config: {
+      contentTypes: [
+        {
+          uid: "api::portfolio-element.portfolio-element",
+          draft: {
+            query: {
+              type: "portfolio",
+            },
+            targetField: "slug",
+          },
+        },
+        {
+          uid: "api::blog-article.blog-article",
+          draft: {
+            targetField: "slug",
+            query: {
+              type: "blog",
+            },
+          },
+        },
+      ],
+    },
+  },
 });
