7fbc45ba10d6c26b8f00e2ba0f1ef1d92e4e4d69
Nagy Istvan <info@ist1.dev>
2022-10-05 14:16:59 +0200

    SMTP connector added

--- config/plugins.js
+++ config/plugins.js
@@ -2,6 +2,23 @@ module.exports = ({ env }) => ({
   "generate-data": {
     enabled: true,
   },
+  email: {
+    provider: "smtp",
+    providerOptions: {
+      host: "smtp.gmail.com", //SMTP Host
+      port: 465, //SMTP Port
+      secure: true,
+      username: "my.username@gmail.com",
+      password: "my.password",
+      rejectUnauthorized: true,
+      requireTLS: true,
+      connectionTimeout: 1,
+    },
+    settings: {
+      from: "my.username@gmail.com",
+      replyTo: "my.username@gmail.com",
+    },
+  },
   "preview-button": {
     enabled: true,
     config: {
