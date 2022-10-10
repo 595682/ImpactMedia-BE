436d76dd4ea9e9903ee0293ae7f67478deec768d
Nagy Istvan <info@ist1.dev>
2022-10-10 16:25:38 +0200

    email provider strapi-provider-smtp reconfigured

--- config/database.js
+++ config/database.js
@@ -1,17 +1,34 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "mysql",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+module.exports = ({ env }) => {
+  const environment = env("NODE_ENV");
+
+  if (environment === "production") {
+    return {
+      connection: {
+        client: "mysql",
+        connection: {
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USERNAME", "strapi"),
+          password: env("DATABASE_PASSWORD", "strapi"),
+          schema: env("DATABASE_SCHEMA", "public"), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+          },
+        },
+        debug: false,
       },
-    },
-    debug: false,
-  },
-});
+    };
+  } else {
+    return {
+      connection: {
+        client: "sqlite",
+        connection: {
+          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        },
+        useNullAsDefault: true,
+        debug: false,
+      },
+    };
+  }
+};
--- config/plugins.js
+++ config/plugins.js
@@ -3,20 +3,22 @@ module.exports = ({ env }) => ({
     enabled: true,
   },
   email: {
-    provider: "smtp",
-    providerOptions: {
-      host: env("EMAIL_HOST", "smtp.gmail.com"), //SMTP Host
-      port: env("EMAIL_PORT", 123), //SMTP Port
-      secure: true,
-      username: env("EMAIL_USER", "un"),
-      password: env("EMAIL_PASSWORD", "pw"),
-      rejectUnauthorized: true,
-      requireTLS: true,
-      connectionTimeout: 1,
-    },
-    settings: {
-      from: env("EMAIL_FROM_ADDRESS", "my.username@gmail.com"),
-      replyTo: env("EMAIL_REPLYTO_ADDRESS", "my.username@gmail.com"),
+    config: {
+      provider: "strapi-provider-email-smtp",
+      providerOptions: {
+        host: env("EMAIL_HOST", "smtp.gmail.com"), //SMTP Host
+        port: env("EMAIL_PORT", 123), //SMTP Port
+        secure: true,
+        username: env("EMAIL_USER", "un"),
+        password: env("EMAIL_PASSWORD", "pw"),
+        rejectUnauthorized: true,
+        requireTLS: true,
+        connectionTimeout: 1,
+      },
+      settings: {
+        from: env("EMAIL_FROM_ADDRESS", "my.username@gmail.com"),
+        replyTo: env("EMAIL_REPLYTO_ADDRESS", "my.username@gmail.com"),
+      },
     },
   },
   "preview-button": {
