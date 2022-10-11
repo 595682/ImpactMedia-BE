652f6a84d4605eede5b7c69793cf871f6eb6fc16
Nagy Istvan <info@ist1.dev>
2022-10-11 15:17:07 +0200

    DATABASE_SCHEMA added to config/database.js

--- config/database.js
+++ config/database.js
@@ -40,6 +40,7 @@ module.exports = ({ env }) => {
         port: env.int("DATABASE_PORT"),
         database: env("DATABASE_NAME"),
         user: env("DATABASE_USERNAME"),
+        schema: env("DATABASE_SCHEMA", "public"),
         password: env("DATABASE_PASSWORD"),
         ssl: {
           rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
