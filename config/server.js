2c0c329a21b654df4f0dce48de83f7d13a63aa64
Nagy Istvan <info@ist1.dev>
2022-10-12 14:11:39 +0200

    APP_BASE_URL added to config/server

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env.int("APP_BASE_URL", "https://backend.impact-media.eu"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
