618b6994e08840f8150adaa1790429b7ec0c9f14
Nagy Istvan <info@ist1.dev>
2022-10-12 13:05:40 +0200

    Forgor password template added hardcoded

--- config/email-templates/forgot-password.js
+++ config/email-templates/forgot-password.js
@@ -1,10 +1,10 @@
 const subject = `Reset password`;
 
-const html = `<p>Hi <%= from %></p>
-<p>Sorry you lost your password. You can click here to reset it: <%= url %></p>`;
+const html = `<p>Hi user</p>
+<p>Sorry you lost your password. You can click here to reset it: link`;
 
-const text = `Hi <%= from %>
-Sorry you lost your password. You can click here to reset it: <%= url %>`;
+const text = `Hi user
+Sorry you lost your password. You can click here to reset it: link`;
 
 module.exports = {
   subject,
