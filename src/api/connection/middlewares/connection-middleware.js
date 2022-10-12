1a0ca47c8f1a3c32890568ec321be1d89d2af739
Nagy Istvan <info@ist1.dev>
2022-10-12 16:36:57 +0200

    Email alert set up v2

--- src/api/connection/middlewares/connection-middleware.js
+++ src/api/connection/middlewares/connection-middleware.js
@@ -1,3 +1,10 @@
+const emailTemplate = {
+  text: `Welcome to mywebsite.fr!
+      Your account is now linked with: <%= request.email %>.`,
+  html: `<h1>Welcome to mywebsite.fr!</h1>
+      <p>Your account is now linked with: <%= request.email %>.<p>`,
+};
+
 module.exports = () => {
   return async (ctx, next) => {
     console.info(
@@ -5,14 +12,33 @@ module.exports = () => {
     );
     console.log(ctx.request.body);
 
+    /* {
+ctx.request.body= {
+  isQuot:Boolean
+  firstName:String
+  lastName:String
+  email:String
+  description:String
+  service:String
+}
+} */
+
     try {
-      await strapi.plugins["email"].services.email.send({
-        to: "istvan@boostern.com",
-        from: process.env.EMAIL_FROM_ADDRESS,
-        subject: "The Strapi Email plugin worked successfully",
-        text: "Hello world!",
-        html: "Hello world!",
-      });
+      await strapi.plugins["email"].services.email.sendTemplatedEmail(
+        {
+          to: "istvan@boostern.com",
+          from: process.env.EMAIL_FROM_ADDRESS,
+        },
+        {
+          ...emailTemplate,
+          subject: ctx.request.body.isQuot
+            ? "New quotation request from the IMPACT MEDIA site"
+            : "New contact request from the IMPACT MEDIA site",
+        },
+        {
+          request: ctx.request.body,
+        }
+      );
 
       console.log("Alert email sent");
     } catch (e) {
