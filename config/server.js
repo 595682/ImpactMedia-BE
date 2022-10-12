51b11b630df6aadac4b0c8ce5eedb85079212619
Nagy Istvan <info@ist1.dev>
2022-10-12 14:35:46 +0200

    app url hardcoded config/server

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env.int("APP_BASE_URL", "https://backend.impact-media.eu"),
+  url: "https://backend.impact-media.eu",
   app: {
     keys: env.array("APP_KEYS"),
   },
